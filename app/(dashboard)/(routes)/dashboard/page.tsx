d231f6c80d2eba16271b4f0c32ff736887dc9101
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-07-12 01:42:59 +0530

    chore: Add dashboard page component

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { UserProfile } from "@clerk/clerk-react";
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <p>Dashboard</p>
+    </div>
+  );
+}
