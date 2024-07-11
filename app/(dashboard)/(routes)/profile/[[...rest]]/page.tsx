4933768d827e513a82419b09a102bcddfe059206
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-07-12 01:43:04 +0530

    feat: Add profile page component

---
+++ app/(dashboard)/(routes)/profile/[[...rest]]/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { UserProfile } from "@clerk/nextjs";
+import React from "react";
+
+export default function Profile() {
+  return (
+    <div>
+      <UserProfile />
+    </div>
+  );
+}
