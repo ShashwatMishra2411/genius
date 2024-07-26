567c1332f1368afc52f0bf4c543468829476f201
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-07-27 00:43:14 +0530

    chore: Remove unused Button import from Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
 import { Montserrat } from "next/font/google";
