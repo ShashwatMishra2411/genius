b53f98c7edac0ccc083120069a37c84e67e04763
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-08-02 19:01:01 +0530

    feat: Add formSchema3 and VideoDetails type
    This commit adds the formSchema3 object and the VideoDetails type to the types.tsx file. The formSchema3 object includes a link property with a minimum length validation, and the VideoDetails type includes properties for title, thumbnailUrl, transcript, summary, clickbait, and videoURL. These additions enhance the functionality of the application by providing support for video-related data and validation.

--- types.tsx
+++ types.tsx
@@ -14,7 +14,7 @@ export type ChatCompletionMessageParam = {
 export type ImageChat = {
   role: string;
   imageUrl: string;
-}
+};
 
 export const formSchema2 = z.object({
   prompt: z.string().min(1, {
@@ -23,3 +23,18 @@ export const formSchema2 = z.object({
   amount: z.string().min(1),
   resolution: z.string().min(1),
 });
+
+export const formSchema3 = z.object({
+  link: z.string().min(1, {
+    message: "Prompt is required",
+  }),
+});
+
+export type VideoDetails = {
+  title: string;
+  thumbnailUrl: string;
+  transcript: string;
+  summary: string;
+  clickbait: string;
+  videoURL: string;
+};
