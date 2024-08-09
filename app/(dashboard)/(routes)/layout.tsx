97a9ecf25738d10d216151e286f6393b5c9906aa
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-08-09 16:50:17 +0530

    feat: Update layout component with fixed sidebar and progress bar
    This commit updates the layout component in the dashboard routes to include a fixed sidebar and a progress bar. The sidebar is now fixed on the left side of the screen for larger screens, improving navigation accessibility. Additionally, a progress bar is added to the sidebar, displaying the current progress based on the API limit count. These changes enhance the user experience and provide better visibility of the application's state.

--- app/(dashboard)/(routes)/layout.tsx
+++ app/(dashboard)/(routes)/layout.tsx
@@ -1,19 +1,21 @@
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import { getCount } from "@/lib/api-limit";
 import React from "react";
 
-export default function DashLayout({
+export default async function DashLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const progress = (await getCount()) ?? 0;
   return (
-    <section className="min-h-screen flex">
-      <div className="min-h-screen relative bg-gray-900 hidden md:flex md:fex-col md:inset-y-0 z-[80] w-72">
-        <Sidebar />
+    <section className="min-h-screen max-w-screen w-full overflow-hidden flex">
+      <div className="min-h-screen fixed bg-gray-900 hidden lg:flex lg:fex-col lg:inset-y-0 w-60">
+        <Sidebar progress={progress} />
       </div>
-      <div className="flex flex-col w-full">
-        <Navbar />
+      <div className="flex flex-col overflow-hidden h-screen lg:pl-60 w-full">
+        <Navbar progress={progress} />
         {children}
       </div>
     </section>
